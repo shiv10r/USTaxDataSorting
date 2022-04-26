aa7eb284b7e292e79c2407c28328fef4ea83ee5e
shiv10r <shiv99stark@gmail.com>
2022-04-26 15:31:58 +0530

    Initial   commit

---
+++ src/com/shrgfg/CountryTax.java
@@ -0,0 +1,33 @@
+package com.shrgfg;
+
+
+ class CountyTax {
+    private double tax;
+    private String county;
+
+    public CountyTax() {
+        tax = 0;
+        county = "";
+    }
+
+    public CountyTax(double newTax, String newCounty) {
+        tax = newTax;
+        county = newCounty;
+    }
+
+    public void setTax(double newTax) {
+        tax = newTax;
+    }
+
+    public void setCounty(String newCounty) {
+        county = newCounty;
+    }
+
+    public double getTax() {
+        return tax;
+    }
+
+    public String getCounty() {
+        return county;
+    }
+}
---
+++ src/com/shrgfg/Main.java
@@ -0,0 +1,5 @@
+package com.shrgfg;
+
+public class Main {
+
+}
